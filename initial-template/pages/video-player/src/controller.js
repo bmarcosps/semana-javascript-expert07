42e95c5841fe5e4043a154a21925029cd85ecde7
bmarcosps <brunomarcospsilva@gmail.com>
2023-03-05 19:08:25 -0300

    first class

--- initial-template/pages/video-player/src/controller.js
+++ initial-template/pages/video-player/src/controller.js
@@ -1,10 +1,66 @@
 export default class Controller {
-	constructor({}) {}
+	#view;
+	#camera;
+	#worker;
+	#blinkCount = 0;
+	constructor({ view, camera, worker }) {
+		this.#view = view;
+		this.#camera = camera;
+		this.#worker = this.#configureWorker(worker);
+
+		this.#view.configureOnBtnClick(this.onBtnStart.bind(this));
+	}
 
 	static async initialize(deps) {
 		const controller = new Controller(deps);
+		controller.log('Not yet initialized. Click the button to start');
 		return controller.init();
 	}
 
-	async init({}) {}
+	#configureWorker(worker) {
+		let ready = false;
+		console.log('configuring worker', worker);
+		worker.onmessage = ({ data }) => {
+			console.log('controller received message', data);
+			if (data === 'READY') {
+				this.#view.enableButton();
+				ready = true;
+				return;
+			}
+			const blinked = data.blinked;
+			this.#blinkCount += blinked;
+			this.#view.togglePlayVideo();
+			console.log('blinked', blinked);
+		};
+		return {
+			send(msg) {
+				if (!ready) return;
+				worker.postMessage(msg);
+			},
+		};
+	}
+
+	async init() {
+		console.log('init');
+	}
+
+	loop() {
+		const video = this.#camera.video;
+		const img = this.#view.getVideoFrame(video);
+		this.#worker.send(img);
+		this.log('detecting faces...');
+
+		setTimeout(() => this.loop(), 100);
+	}
+
+	log(txt) {
+		const times = `    - blinked times: ${this.#blinkCount}`;
+		this.#view.log(`logger: ${txt}`.concat(this.#blinkCount ? times : ''));
+	}
+
+	onBtnStart() {
+		this.log('Starting...');
+		this.#blinkCount = 0;
+		this.loop();
+	}
 }
--- initial-template/pages/video-player/src/factory.js
+++ initial-template/pages/video-player/src/factory.js
@@ -3,20 +3,49 @@ import { supportsWorkerType } from '../../../lib/shared/util.js';
 import Controller from './controller.js';
 import Service from './service.js';
 import View from './view.js';
-if (supportsWorkerType()) {
-	console.log('Worker type supported');
-} else {
-	console.log('Worker type not supported');
+async function getWorker() {
+	console.log('supportsWorkerType', supportsWorkerType());
+	if (supportsWorkerType()) {
+		const worker = new Worker('./src/worker.js', { type: 'module' });
+		return worker;
+	}
+	console.warn('Web Workers are not supported in this browser');
+	console.warn('Loading TensorFlow.js dependencies manually...');
+	await import('https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js');
+	await import('https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js');
+	await import('https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js');
+	await import('https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js');
+	console.warn('Using a mock instead.');
+	const service = new Service({
+		faceLandmarksDetection: window.faceLandmarksDetection,
+	});
+	const workerMock = {
+		async postMessage(video) {
+			const blinked = await service.blinked(video);
+			if (!blinked) return;
+			workerMock.onmessage({ data: { blinked } });
+		},
+		// vai ser sobrescrito pelo controller
+		onmessage() {},
+	};
+	console.log('loading tf model...');
+	await service.loadModel();
+	console.log('tf model loaded');
+	setTimeout(() => workerMock.onmessage({ data: 'READY' }), 500);
+	return workerMock;
 }
+
+const worker = await getWorker();
 
 const camera = await Camera.init();
 const cardListWorker = new Worker('./src/workers/cardListWorker.js', { type: 'module' });
 const [rootPath] = window.location.href.split('/pages/');
 const factory = {
-	async initalize() {
+	async initialize() {
 		return Controller.initialize({
-			view: new View({}),
-			service: new Service({}),
+			view: new View(),
+			camera,
+			worker,
 		});
 	},
 };
